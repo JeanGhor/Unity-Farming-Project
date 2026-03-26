ee8523fd8a5edeaf16fd6b1d39325087a075e428
JeanGhor <jjrjgh@gmail.com>
2026-03-26 14:33:16 +0200

    New time system, with UI element

--- Assets/Scripts/TimeEconomy/TimeSystem.cs
+++ Assets/Scripts/TimeEconomy/TimeSystem.cs
@@ -1,41 +1,69 @@
-using UnityEngine; 
+using UnityEngine;
+using TMPro;
 
 public class TimeSystem : MonoBehaviour
 {
-    float SECONDS_IN_MINUTE = 1f;
+    [Header("Time Speed")]
+    [SerializeField] private float secondsPerGameMinute = 1f;
 
     [Header("Start Time")]
     [SerializeField] private int _startHours = 8;
     [SerializeField] private int _startMinutes = 0;
+
     [Header("Current Time")]
     [SerializeField] private int _hours;
     [SerializeField] private int _minutes;
     [SerializeField] private int _days;
-    private float _timer=0f;
+
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI timeText;
+
+    private float _timer;
+
     void Start()
     {
         _hours = _startHours;
         _minutes = _startMinutes;
-        _days = 0;
+        _days = 1;
+
+        UpdateTimeUI();
     }
+
     void Update()
     {
         _timer += Time.deltaTime;
-        Debug.Log($"Time: {_hours:00}:{_minutes:00} Day: {_days}");
-        if (_timer >= SECONDS_IN_MINUTE)
+
+        if (_timer >= secondsPerGameMinute)
         {
-            _timer = 0f;
-            _minutes++;
-            if (_minutes >= 60)
+            _timer -= secondsPerGameMinute;
+            AdvanceMinute();
+        }
+    }
+
+    private void AdvanceMinute()
+    {
+        _minutes++;
+
+        if (_minutes >= 60)
+        {
+            _minutes = 0;
+            _hours++;
+
+            if (_hours >= 24)
             {
-                _minutes = 0;
-                _hours++;
-                if (_hours >= 24)
-                {
-                    _hours = 0;
-                    _days++;
-                }
+                _hours = 0;
+                _days++;
             }
+        }
+
+        UpdateTimeUI();
+    }
+
+    private void UpdateTimeUI()
+    {
+        if (timeText != null)
+        {
+            timeText.text = $"Day {_days} - {_hours:00}:{_minutes:00}";
         }
     }
 }
